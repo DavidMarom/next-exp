635b15d64b2873808e39e386a363cb5af0991818
David Marom <davidmarom.pro@gmail.com>
2023-06-21 19:18:55 +0300

    removed getAllDocNames

--- pages/utils/db.js
+++ pages/utils/db.js
@@ -16,19 +16,3 @@ export async function getAllDocuments(client, collection) {
   return documents;
 }
 
-export async function getAllCollectionNames() {
-    const uri = client.db();
-    const client = new MongoClient(uri);
-  
-    try {
-      await client.connect();
-  
-      const database = client.db('bold'); // Replace with your database name
-  
-      const collections = await database.listCollections().toArray();
-      const collectionNames = collections.map((collection) => collection.name);
-      return collectionNames;
-    } finally {
-      await client.close();
-    }
-  }
