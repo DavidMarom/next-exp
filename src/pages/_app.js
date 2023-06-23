b1e758ff975645ec60dedcf4f4a60925a5174c0f
David Marom <davidmarom.pro@gmail.com>
2023-06-23 10:24:50 +0300

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,11 @@
-import Layout from "../components/layout/layout";
-import "../styles/globals.css";
-
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
       <title>The Bold Developers</title>
       <div className="content-container">
         <Component {...pageProps} />
       </div>
-    </Layout>
+    </>
   );
 }
 
