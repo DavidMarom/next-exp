51a6c892cac8ddb108af0afa66e236c6202c6108
David Marom <davidmarom.pro@gmail.com>
2023-06-30 09:58:03 +0300

    Added insertDocument to back and client

--- src/app/david/page.js
+++ src/app/david/page.js
@@ -8,6 +8,9 @@ const David = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   useEffect(() => { getData() }, []);
 
   const getData = async () => {
@@ -26,6 +29,16 @@ const David = () => {
         <h1>Calling mongoDB</h1>
         {loading ? <p>Calling mongoDB...</p> : <p>Done:</p>}
         {data.map((item, index) => <div key={index}><p>{item.name} - {item.price}</p> </div>)}
+        <br />
+        <h1>Inserting data</h1>
+        <p>Name: </p><input type="text" onChange={(e) => setName(e.target.value)} />
+        <p>Price: </p><input type="number" onChange={(e) => setPrice(e.target.value)} />
+        <button onClick={async () => {
+          const res = await http.post("/api01", { name, price });
+          console.log(res);
+          getData();
+        }}>Insert</button>
+        
       </PageContainer>
     </Row>
 
--- src/pages/api/api01.js
+++ src/pages/api/api01.js
@@ -1,4 +1,4 @@
-import { connectDatabase, getAllDocuments } from "../../services/db";
+import { connectDatabase, getAllDocuments, insertDocument } from "../../services/db";
 
 async function handler(req, res) {
   if (req.method === "GET") {
@@ -8,6 +8,20 @@ async function handler(req, res) {
       res.status(200).json(documents);
     } catch (error) { res.status(500).json({ message: "Connecting to the database failed!" }) }
   }
+
+  if (req.method === "POST") {
+    const { name, price } = req.body;
+    if (!name || !price) { return res.status(422).json({ message: "Invalid input." }) }
+    const newDocument = { name, price };
+    try {
+      const client = await connectDatabase();
+      const result = await insertDocument(client, "items", newDocument);
+      newDocument._id = result.insertedId;
+      res.status(201).json({ message: "Inserted document.", document: newDocument });
+    }
+    catch (error) { res.status(500).json({ message: "Inserting data failed!" }) }
+  }
+
 }
 
 export default handler;
