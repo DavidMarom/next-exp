6982f97447838a843e3b155f28c759e52f849831
Noam <noams299@gmail.com>
2023-06-30 12:07:17 +0300

    added noams page

--- src/app/noams/page.js
+++ src/app/noams/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <PageContainer>
 
         <h1>Noams Page</h1>
-        <p>I'm on my journey to become a professional Full-Stack developer</p>
+        <p>I am on my journey to become a professional Full-Stack developer</p>
         <br />
         <b><p>Reach me on:</p></b>
         <a href="https://www.linkedin.com/in/noam-sadeh/" target="_blank">Linkedin</a>
